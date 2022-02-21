9f1cc2650bf3487bb05959e4df1539a579d4e3a9
pratikgirikonvergedotai <pratik.giri@konverge.ai>
2022-02-21 11:11:31 +0530

    heap

--- heap/file.js
+++ heap/file.js
@@ -23,3 +23,30 @@ class Minheap{
         }
     }
 }
+
+
+class Maxheap{
+    constructor(){
+        this.heap = [null]
+    }
+    
+    getMax(){
+        return this.heap[-1]
+    }
+
+    add(data){
+        this.heap.push(data)
+
+
+        if (this.heap.length > 1){
+
+            let current = this.heap.length - 1
+            // checking if parent node is greater than child node. traversing up
+            while (current > 1 && this.heap[Math.floor(current/2)] < this.heap[current]){
+                // swapping smaller nodes
+                [this.heap[Math.floor(current/2)], this.heap[current]] = [this.heap[current], this.heap[Math.floor(current/2)]]
+                current = Math.floor(current/2)
+            }
+        }
+    }
+}
