0faaff66d00c3ed5ba9040dcaac54134c599b4b8
pratikgirikonvergedotai <pratik.giri@konverge.ai>
2022-02-21 11:12:49 +0530

    heap

--- heap/file.js
+++ heap/file.js
@@ -41,9 +41,9 @@ class Maxheap{
         if (this.heap.length > 1){
 
             let current = this.heap.length - 1
-            // checking if parent node is greater than child node. traversing up
+            // checking if parent node is less than child node. traversing up
             while (current > 1 && this.heap[Math.floor(current/2)] < this.heap[current]){
-                // swapping smaller nodes
+                // swapping greater nodes
                 [this.heap[Math.floor(current/2)], this.heap[current]] = [this.heap[current], this.heap[Math.floor(current/2)]]
                 current = Math.floor(current/2)
             }
